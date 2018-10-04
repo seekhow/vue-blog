7225ebaa6ac87480f5063748af1aba092753fb16
seekhow <seekhowqiuhao@gmail.com>
2018-10-04 20:40:47 +0800

    feat: admin页面右上角添加打开home页面

--- client/src/modules/admin/components/Admin.vue
+++ client/src/modules/admin/components/Admin.vue
@@ -1,54 +1,46 @@
 <template>
-    <div class="admin">
-        <div class="admin__list">
-            <list></list>
-        </div>
-        <div class="admin__editor">
-            <editor></editor>
-        </div>
-        <div class="admin__logout">
-            <i class="fa fa-power-off" aria-hidden="true" @click="logout"></i>
-        </div>
+  <div class="admin">
+    <div class="admin__list">
+      <list></list>
     </div>
+    <div class="admin__editor">
+      <editor></editor>
+    </div>
+    <div class="admin__top-menu">
+      <a href="http://blog.seekhow.cn" target="__blank"><i class="fa fa-home admin__top-menu__icon" aria-hidden="true"></i></a>
+      <i class="fa fa-power-off" aria-hidden="true" @click="logout"></i>
+    </div>
+  </div>
 </template>
 
 <script>
 import Editor from './Editor.vue';
 import List from './List.vue';
 import {
-    mapMutations,
+  mapMutations,
 } from 'vuex';
 
 export default {
-    name: 'admin',
-    components: {
-        Editor,
-        List,
-    },
-    data() {
-        return {};
-    },
-    created() {
-
-    },
-    methods: {
-        ...mapMutations({
-            deleteToken: 'DELETE_TOKEN',
-        }),
-        logout() {
-            this.$confirm('此操作将退出系统, 是否继续?', '提示', {
-                confirmButtonText: '确定',
-                cancelButtonText: '取消',
-                type: 'warning',
-            }).then(() => {
-                this.deleteToken();
-                this.$router.push('/admin/login');
-            }).catch(() => {
-
-            });
-        },
-    },
-    computed: {},
+  name: 'admin',
+  components: {
+    Editor,
+    List,
+  },
+  methods: {
+    ...mapMutations({
+      deleteToken: 'DELETE_TOKEN',
+    }),
+    logout() {
+      this.$confirm('此操作将退出系统, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning',
+      }).then(() => {
+        this.deleteToken();
+        this.$router.push('/admin/login');
+      }).catch();
+    }
+  }
 };
 </script>
 
@@ -65,11 +57,14 @@ export default {
     overflow-x hidden
   &__editor
     margin-left 500px
-  &__logout
+  &__top-menu
     position absolute
     top 22px
     right 30px
     font-size 28px
     cursor pointer
     color $blue
+    &__icon
+      margin 0px 20px
+      color $blue
 </style>
